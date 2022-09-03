a64438765b8973f725d57e3e5efc8a3b9ddd6bf3
abuthahir-19 <abuthahirstar1@gmail.com>
2022-09-03 20:47:50 +0530

    .

--- Sample.java
+++ Sample.java
@@ -1,15 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Sample {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        List <Integer> path = new ArrayList<>();
-        path.add(7);
-
-        List <Integer> newPath = new ArrayList<>(path);
-        System.out.println(newPath);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = in.nextInt();
+        }
+        int s = in.nextInt();
+        int e = in.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if ((ar[i] >= s && ar[i] <= e) && ar[i] % 3 != 0 && ar[i] % 5 != 0) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
         in.close();
     }
 }
--- SecondLargestEven.java
+++ SecondLargestEven.java
@@ -6,14 +6,19 @@ import java.util.List;
 public class SecondLargestEven {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int n = 4;
         List <Integer> evens = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             int val = in.nextInt();
-            if(val % 2 == 0) evens.add(val);
+            if (val % 2 == 0) evens.add(val);
         }
-        Collections.sort(evens);
-        System.out.println (evens.get(1));
+        int c = 1;
+        while (c != 0) {
+            int max = Collections.max(evens);
+            evens.remove(evens.indexOf(max));
+            c-=1;
+        }
+        System.out.println(Collections.max(evens));
         in.close();
     }
 }
